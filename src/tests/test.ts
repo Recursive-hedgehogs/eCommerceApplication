e28b2c279f1880df6f27e01c34b11ab7fea27b1d
Alina <astapchik.1993@mail.ru>
2023-08-07 18:15:49 +0300

    fix: fix project config

--- src/tests/test.ts
+++ src/tests/test.ts
@@ -1,10 +1,10 @@
+import * as packageJson from '../../package.json';
+
 test('Husky is configured in package.json', () => {
-    const packageJson = require('../../package.json');
     expect(packageJson.husky).toBeDefined();
 });
 
 test('Husky is installed and configured properly', () => {
-    const packageJson = require('../../package.json');
     const huskyConfig = packageJson.husky;
     expect(huskyConfig.hooks['pre-commit']).toBeDefined();
     expect(huskyConfig.hooks['pre-commit']).toBe('lint');
