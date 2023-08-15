a6b47e9da570d14ff230d2a215254d4a70505dcc
Olitera <lu_gu@mail.ru>
2023-08-15 14:02:27 +0200

    feat: add submit on registration form

--- src/controllers/controllers.ts
+++ src/controllers/controllers.ts
@@ -1,5 +1,6 @@
 import App from '../app/app';
 import { ROUTE } from '../models/enums/enum';
+import { apiCustomer } from '../api/api-customer';
 
 export class Controllers {
     private app: App | null;
@@ -27,6 +28,7 @@ export class Controllers {
         });
 
         this.app?.view?.pages?.get(ROUTE.MAIN)?.addEventListener('click', this.onMainPageClick);
+        this.app?.view?.pages?.get(ROUTE.REGISTRATION)?.addEventListener('submit', this.onRegistrationSubmit);
     }
 
     private onMainPageClick = (e: Event): void => {
@@ -70,5 +72,37 @@ export class Controllers {
     private redirectCallBack = (e: PopStateEvent): void => {
         const currentPath: string = window.location.pathname.slice(1);
         this.app?.setCurrentPage(currentPath, e.state.page === currentPath);
+    };
+
+    private onRegistrationSubmit = (e: Event): void => {
+        const target: EventTarget | null = e.target;
+        if (target instanceof HTMLFormElement) {
+            e.preventDefault();
+            const fields: NodeListOf<HTMLInputElement> = target.querySelectorAll('.form-item input');
+            const fieldNames: string[] = [
+                'email',
+                'password',
+                'firstName',
+                'lastName',
+                'dateOfBirth',
+                'country',
+                'city',
+                'streetName',
+                'postalCode',
+            ];
+            const pairs: string[][] = [...fields].map((el: HTMLInputElement, i: number) => [fieldNames[i], el.value]);
+            const address = Object.fromEntries(pairs.slice(5));
+            const customerData = Object.fromEntries(pairs.slice(0, 5));
+
+            address.country = this.app?.getCodeFromCountryName(address.country);
+            customerData.addresses = [address];
+
+            apiCustomer
+                .createCustomer(customerData)
+                .then((resp): void => {
+                    console.log(resp);
+                })
+                .catch((err: Error) => alert(err.message));
+        }
     };
 }
