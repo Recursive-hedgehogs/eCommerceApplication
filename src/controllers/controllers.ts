110f55c326ceae5fe20871ca9bad551b1054e5b6
Siarhei1927 <shaurylau@gmail.com>
2023-08-23 12:26:28 +0200

    feat: add jest-fetch-mode

--- src/controllers/controllers.ts
+++ src/controllers/controllers.ts
@@ -14,6 +14,7 @@ import SdkAuth from '@commercetools/sdk-auth';
 import { environment } from '../environment/environment';
 import { ApiExistingTokenFlow } from '../api/api-existing-token-flow';
 import { ITokenResponse } from '../models/interfaces/interface';
+import fetch from 'node-fetch';
 
 export class Controllers {
     private app: App | null;
