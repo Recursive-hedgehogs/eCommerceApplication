2e1d69c30fa1f1dd4fe6639380f7665846646131
Siarhei1927 <shaurylau@gmail.com>
2023-08-23 12:27:57 +0200

    feat: add app tests like done and controllers tests like commets

--- src/tests/test.ts
+++ src/tests/test.ts
@@ -1,9 +1,9 @@
 import * as packageJson from '../../package.json';
 import App from '../app/app';
 import View from '../view/view';
-//import { Controllers } from '../controllers/controllers';
 import { ROUTE } from '../models/enums/enum';
-//import { apiCustomer } from '../api/api-customer';
+import { Controllers } from '../controllers/controllers';
+import 'jest-fetch-mock';
 
 test('Husky is configured in package.json', () => {
     expect(packageJson.husky).toBeDefined();
@@ -15,41 +15,104 @@ test('Husky is installed and configured properly', () => {
     expect(huskyConfig.hooks['pre-commit']).toBe('lint');
 });
 
-test('setCurrentPage redirects to MAIN when authenticated user goes to LOGIN', () => {
-    const app: App = new App();
-    app.setAuthenticationStatus(true);
-    // const setCurrentPageSpy = jest.spyOn(app, 'setCurrentPage');
+describe('App', () => {
+    let app: App;
 
-    app.setCurrentPage(ROUTE.LOGIN);
+    beforeEach(() => {
+        app = new App();
+    });
 
-    // expect(setCurrentPageSpy).toHaveBeenCalledWith(ROUTE.MAIN);
+    test('setAuthenticationStatus sets authentication status', () => {
+        app.setAuthenticationStatus(true);
+        expect(app.isAuthenticated()).toBe(true);
+    });
+
+    test('isAuthenticated returns correct authentication status', () => {
+        app.setAuthenticationStatus(false);
+        expect(app.isAuthenticated()).toBe(false);
+        app.setAuthenticationStatus(true);
+        expect(app.isAuthenticated()).toBe(true);
+    });
+
+    test('start sets the view property', () => {
+        const mockView: View = {} as View;
+        app.start(mockView);
+        expect(app.view).toBe(mockView);
+    });
+
+    test('App authentication status is initially set to false', () => {
+        expect(app.isAuthenticated()).toBe(false);
+    });
+
+    test('App authentication status can be toggled', () => {
+        app.setAuthenticationStatus(false);
+        expect(app.isAuthenticated()).toBe(false);
+        app.setAuthenticationStatus(true);
+        expect(app.isAuthenticated()).toBe(true);
+    });
+
+    test('App starts with correct initial view properties', () => {
+        const mockView: View = {} as View;
+        app.start(mockView);
+        expect(app.view).toBe(mockView);
+    });
+
+    test('setCurrentPage redirects to MAIN when authenticated user goes to LOGIN', () => {
+        const app: App = new App();
+        app.setAuthenticationStatus(true);
+        app.setCurrentPage(ROUTE.LOGIN);
+    });
 });
 
-test('setAuthenticationStatus sets authentication status', () => {
-    const app = new App();
+/*describe('Controllers', () => {
+    let mockApp: App;
+    let controllers: Controllers;
 
-    app.setAuthenticationStatus(true);
+    beforeEach(() => {
+        mockApp = new App();
+        controllers = new Controllers();
+        controllers.start(mockApp);
+    });
 
-    expect(app.isAuthenticated()).toBe(true);
-});
+    test('addListeners adds event listeners correctly', () => {
+        const loginBtn = document.createElement('button');
+        loginBtn.id = 'login-btn';
+        document.body.appendChild(loginBtn);
 
-test('isAuthenticated returns correct authentication status', () => {
-    const app: App = new App();
+        const logoutBtn = document.createElement('button');
+        logoutBtn.id = 'logout-btn';
+        document.body.appendChild(logoutBtn);
 
-    app.setAuthenticationStatus(false);
+        const registrBtn = document.createElement('button');
+        registrBtn.id = 'registration-btn';
+        document.body.appendChild(registrBtn);
 
-    expect(app.isAuthenticated()).toBe(false);
+        const logoLink = document.createElement('a');
+        logoLink.classList.add('navbar-brand');
+        document.body.appendChild(logoLink);
 
-    app.setAuthenticationStatus(true);
+        fetchMock.mockResponse(JSON.stringify({ data: 'some response data' }));
 
-    expect(app.isAuthenticated()).toBe(true);
-});
+        controllers.addListeners();
 
-test('start sets the view property', () => {
-    const app: App = new App();
-    const mockView: View = {} as View;
+        loginBtn.click();
+        expect(mockApp.setCurrentPage).toHaveBeenCalledWith('login');
 
-    app.start(mockView);
+        logoutBtn.click();
+        expect(mockApp.setAuthenticationStatus).toHaveBeenCalledWith(false);
+        expect(mockApp.setCurrentPage).toHaveBeenCalledWith('login');
+        expect(logoutBtn.classList.contains('hidden')).toBe(false);
+        expect(loginBtn.classList.contains('hidden')).toBe(true);
 
-    expect(app.view).toBe(mockView);
-});
+        registrBtn.click();
+        expect(mockApp.setCurrentPage).toHaveBeenCalledWith('registration');
+
+        logoLink.click();
+        expect(mockApp.setCurrentPage).toHaveBeenCalledWith('main');
+
+        document.body.removeChild(loginBtn);
+        document.body.removeChild(logoutBtn);
+        document.body.removeChild(registrBtn);
+        document.body.removeChild(logoLink);
+    });
+});*/
